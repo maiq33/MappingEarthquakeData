a8b1fdc0d2a12f257a265ed09a25b9b86ba4f34c
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-08-25 03:19:40 +0000

    primer version

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
-function setup(){
+let leafletMap;
 
+function setup(){
+    noCanvas(); // Como no vamos a dibujar con p5.js en este caso
+    initMap();  // Inicializar el mapa
 }
 
 function draw(){
-    
+    // Si tienes código relacionado con p5.js, lo pones aquí.
 }
+
+function initMap() {
+    leafletMap = L.map('leafletMapDiv').setView([0, 0], 2);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(leafletMap); 
+}
+
