9d5a8a5e84c9e79924014cddb53c8f23e0360855
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-08-25 03:28:57 +0000

    primer commit

--- sketch.js
+++ sketch.js
@@ -10,9 +10,23 @@ function draw(){
 }
 
 function initMap() {
-    leafletMap = L.map('leafletMapDiv').setView([0, 0], 2);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(leafletMap); 
+    // Crear el mapa y desactivar interactividad
+    leafletMap = L.map('leafletMapDiv', {
+        center: [0, 0],
+        zoom: 2,
+        draggable: false,   // Desactivar movimiento con drag
+        zoomControl: false, // Desactivar controles de zoom
+        scrollWheelZoom: false, // Desactivar zoom con rueda del mouse
+        touchZoom: false,   // Desactivar zoom con gestos táctiles
+        doubleClickZoom: false // Desactivar zoom con doble clic
+    });
+
+    // Agregar un "tileset" con diseño oscuro (en este caso, uno de CartoDB)
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
+    }).addTo(leafletMap);
 }
+
 
