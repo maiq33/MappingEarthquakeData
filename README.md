4d69776f34684c603d05faeadd549319cdd2af47
Miguel Angel Treviño Gonzalez <46172668+maiq33@users.noreply.github.com>
2023-08-26 01:44:57 +0000

    introduciendo datos de los terremotos

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 este es el programa que estoy usando para mostrar el mapa:
 https://leafletjs.com/
+
+De aqui obtengo los datos mundiales de terremotos:
+https://www.usgs.gov/programs/earthquake-hazards
+https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+
+Video tutorial - minuto 20:40
+https://www.youtube.com/watch?v=ZiYdOwOrGyc&list=PLRqwX-V7Uu6a-SQiI4RtIwuOrLJGnel0r&index=11&t=785s
--- sketch.js
+++ sketch.js
@@ -1,24 +1,64 @@
 let leafletMap;
 
+let clat = 0;
+let clon = 0;
+
+// 31.2303904, 121.4737021 - Shanghai 
+let lat = 31.2303904;
+let lon = 121.4737021;
+
+let mapZoom = 2;
+
+let earthquakes;
+
+function preload(){
+    earthquakes = loadStrings("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv")
+}
+
 function setup(){
     noCanvas(); // Como no vamos a dibujar con p5.js en este caso
     initMap();  // Inicializar el mapa
+
+    // Agregar un círculo a las coordenadas de Shanghai
+    L.circle([lat, lon], {
+        color: 'magenta',
+        fillColor: 'magenta',
+        fillOpacity: 0.5,
+        radius: 50000  // radio en metros, puedes ajustar según necesites
+    }).addTo(leafletMap);
+
+    for (let i = 0; i < earthquakes.length; i++) {
+        let data = earthquakes[i].split(/,/);
+        print(data);
+        
+    }
 }
 
-function draw(){
-    // Si tienes código relacionado con p5.js, lo pones aquí.
+function mercX(lon){
+    lon = radiasn(lon);
+    let a = (256 / PI) * pow(2,mapZoom);
+    let b = a + PI;
+    return a * b;
+}
+
+function mercY(lat){
+    lat = radians(lat);
+    let a = (256 / PI) * pow(2,mapZoom);
+    let b = tan((PI / 4) + (lat / 2));
+    let c = PI - log(b)
+    return a * c;
 }
 
 function initMap() {
     // Crear el mapa y desactivar interactividad
     leafletMap = L.map('leafletMapDiv', {
         center: [0, 0],
-        zoom: 2,
-        draggable: false,   // Desactivar movimiento con drag
-        zoomControl: false, // Desactivar controles de zoom
-        scrollWheelZoom: false, // Desactivar zoom con rueda del mouse
-        touchZoom: false,   // Desactivar zoom con gestos táctiles
-        doubleClickZoom: false // Desactivar zoom con doble clic
+        zoom: mapZoom,
+        //draggable: false,   // Desactivar movimiento con drag
+        //zoomControl: false, // Desactivar controles de zoom
+        //scrollWheelZoom: false, // Desactivar zoom con rueda del mouse
+        //touchZoom: false,   // Desactivar zoom con gestos táctiles
+        //doubleClickZoom: false // Desactivar zoom con doble clic
     });
 
     // Agregar un "tileset" con diseño oscuro (en este caso, uno de CartoDB)
